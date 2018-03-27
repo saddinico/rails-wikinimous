81e921e1eb211c507cc0743e9684b93eba5e2d6b
Nicolas Saddi <saddi.nicolas@gmail.com>
2018-03-27 17:22:44 -0300

    Rails Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Article.create(title: Faker::Dog.name, content: Faker::Seinfeld.quote)
+end
